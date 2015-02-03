08afe8d769f9738578c3493d02cbbb0763240c85
Matt Greer <matt.e.greer@gmail.com>
2015-02-02 21:20:27 -0700

    simplified ball

--- src/cljs/breakout/cmp/ball.cljs
+++ src/cljs/breakout/cmp/ball.cljs
@@ -1,11 +1,9 @@
-(ns breakout.cmp.ball
-  (:require [reagent.core :refer [atom]]))
+(ns breakout.cmp.ball)
 
 (defn cmp [pos size]
-  (fn [pos size]
-    [:div.ball {:style {:background-image "url(img/tiles.png)"
-                        :position "absolute"
-                        :width (:width size)
-                        :height (:height size)
-                        :top (:y pos)
-                        :left (:x pos)}}]))
+  [:div.ball {:style {:background-image "url(img/tiles.png)"
+                      :position "absolute"
+                      :width (:width size)
+                      :height (:height size)
+                      :top (:y pos)
+                      :left (:x pos)}}])
