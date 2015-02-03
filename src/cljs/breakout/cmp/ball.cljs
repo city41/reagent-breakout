02eb4d9d8002fedd8bf83c77769418b12a35095e
Matt Greer <matt.e.greer@gmail.com>
2015-02-02 17:23:42 -0700

    ball now animated with css, fixes #2

--- src/cljs/breakout/cmp/ball.cljs
+++ src/cljs/breakout/cmp/ball.cljs
@@ -1,17 +1,9 @@
 (ns breakout.cmp.ball
   (:require [reagent.core :refer [atom]]))
 
-(def frame (atom 0))
-
-(defn move-frame []
-  (reset! frame (mod (inc @frame) 4))
-  (.setTimeout js/window move-frame 100))
-
 (defn cmp [pos size]
-  (move-frame)
   (fn [pos size]
     [:div.ball {:style {:background-image "url(img/tiles.png)"
-                        :background-position [(- -48 (* @frame 16)) -64]
                         :position "absolute"
                         :width (:width size)
                         :height (:height size)
